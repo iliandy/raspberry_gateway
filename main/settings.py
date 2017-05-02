395eea8b93f15dcd3e5bea3a1ff2d0f04abdcc59
iliandy <iandyli@gmail.com>
2017-05-02 14:26:19 -0500

    changed home.html open/close door buttons to single operate door button that dynamically changes, along with urls.py, views.py to function correctly. Also, added logout feature that will close door when user logs out.

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')5t_h#_fks9dafz(cwehbav42#4@xz2@*kmtfsui(6j(98tvpi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
